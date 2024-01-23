9a749935aad51bfb111b20bcc3ffa023179d4494
UnnamedUser <L.UnnamedUser@yandex.ru>
2024-01-24 02:39:55 +0300

    Update Automatization

--- Automatization/PowerProducers.cs
+++ Automatization/PowerProducers.cs
@@ -19,6 +19,7 @@ void Init()
     {
         Monitor = (IMyTextPanel)GridTerminalSystem.GetBlockWithName("Computer Monitor");
         GridTerminalSystem.GetBlocksOfType<IMyPowerProducer>(PowerList, (p) => p.IsWorking);
+        Runtime.UpdateFrequency = UpdateFrequency.Update1;
     }
     catch { Echo("Init error"); }
 }
@@ -32,9 +33,15 @@ public void Main(string argument)
         Init();
         POWER_INIT_COUNTER = 0;
     }
+    if (Monitor == null)
+    {
+        Echo("Monitor not found");
+        Runtime.UpdateFrequency = UpdateFrequency.None;
+    }
 
     Monitor.ContentType = ContentType.NONE;
     Monitor.ContentType = ContentType.SCRIPT;
+    Monitor.ScriptBackgroundColor = Color.Black;
 
     EnergyValue maxOutput = new EnergyValue();
     foreach (IMyPowerProducer powerProducer in PowerList) maxOutput.Value += powerProducer.MaxOutput;
@@ -67,21 +74,25 @@ public void Main(string argument)
             value += Math.Round(maxOutput.Value / 10, 1);
         }
 
-        MySprite Square = new MySprite(SpriteType.TEXTURE, "SquareHollow", new Vector2(50f, 441.75f), new Vector2(20f, 20f), Color.DarkGreen, "", TextAlignment.CENTER, 0f);
-        Frame.Add(Square);
+        for (int w = 0; w < 22; w++)
+            for (int h = 0; h < 11; h++)
+            {
+                MySprite Square = new MySprite(SpriteType.TEXTURE, "SquareHollow", new Vector2(50f + w * 22.5f, 430 - h * 45f), new Vector2(22.5f, 45), new Color(0, 8, 0), "", TextAlignment.CENTER, 0f);
+                Frame.Add(Square);
+            }
 
         MySprite MaxMagnitudeSprite = new MySprite(SpriteType.TEXT, maxOutput.Magnitude, new Vector2(20f, 5f), null, Color.Green, "Debug", TextAlignment.LEFT, 0.4f);
         Frame.Add(MaxMagnitudeSprite);
         
         MySprite VerticalLine = new MySprite(SpriteType.TEXTURE, "SquareSimple", new Vector2(40f, 230f), new Vector2(1f, 445f), Color.Green, "", TextAlignment.CENTER, 0f);
         Frame.Add(VerticalLine);
-        MySprite HorizontalLine = new MySprite(SpriteType.TEXTURE, "SquareSimple", new Vector2(40f, 451.75f), new Vector2(445f, 1f), Color.Green, "", TextAlignment.LEFT, 0f);
+        MySprite HorizontalLine = new MySprite(SpriteType.TEXTURE, "SquareSimple", new Vector2(40f, 451.75f), new Vector2(470f, 1f), Color.Green, "", TextAlignment.LEFT, 0f);
         Frame.Add(HorizontalLine);
 
         for (int i = 0; i < 21; i++)
         {
-            int timeValue = i * 3;
-            float width = 45f + i * 22f;
+            int timeValue = i;
+            float width = 40f + i * 22f;
             MySprite CurrentOutput = new MySprite(SpriteType.TEXT, timeValue.ToString(), new Vector2(width, 453f), null, Color.Green, "Debug", TextAlignment.LEFT, 0.5f);
             Frame.Add(CurrentOutput);
         }
@@ -91,14 +102,14 @@ public void Main(string argument)
         {
             try
             {
-                float width = 45f + plankCount * 22f;
+                float width = 40f + plankCount * 22f;
                 float height = (float)(450 - 435 * (Decemial(ConvertValue(new EnergyValue(energyValue.Value * 100, energyValue.Magnitude)), maxOutput)) / 100);
-                MySprite Mark = new MySprite(SpriteType.TEXTURE, "Circle", new Vector2(width, height), new Vector2(1f, 1f), Color.Yellow, "", TextAlignment.CENTER, 0f);
+                MySprite Mark = new MySprite(SpriteType.TEXTURE, "Circle", new Vector2(width, height), new Vector2(1f, 1f), Color.Red, "", TextAlignment.CENTER, 0f);
                 Frame.Add(Mark);
                 if (plankCount > 0)
                 {
                     int heightPrev = (int)Math.Round(450 - 435 * (Decemial(ConvertValue(new EnergyValue(currentPowerOutputList[plankCount - 1].Value * 100, currentPowerOutputList[plankCount - 1].Magnitude)), maxOutput)) / 100);
-                    MySprite Line = DrawLine(new Vector2((int)Math.Round(width), (int)Math.Round(height)), new Vector2(45 + (plankCount - 1) * 22, heightPrev), 1f, Color.Yellow);
+                    MySprite Line = DrawLine(new Vector2((int)Math.Round(width), (int)Math.Round(height)), new Vector2(40 + (plankCount - 1) * 22, heightPrev), 1f, Color.Yellow);
                     Frame.Add(Line);
                 }
                 plankCount++;
@@ -111,7 +122,7 @@ public void Main(string argument)
     }
 }
 
-List<EnergyValue> AddValue(List<EnergyValue> OutputList, EnergyValue newEnergyValue) // Сделать удаление превышающих точек
+List<EnergyValue> AddValue(List<EnergyValue> OutputList, EnergyValue newEnergyValue)
 {
     OutputList.Insert(0, newEnergyValue);
     return OutputList;
